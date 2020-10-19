e981725f141bd57826623da7a5f75cefb93ddfd7
Yong-Jin-Kim <flyhigh2sky@naver.com>
2020-10-19 23:57:12 +0900

    submit

--- exec.c
+++ exec.c
@@ -49,22 +49,38 @@ exec(char *path, char **argv)
   }
 
   // Load program into memory.
+  // initialize the page table using the information from the header of the ELF
+  // but does not "map page table entries"
+  //uint a;
   sz = 0;
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
     if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
       goto bad;
+    //cprintf("[ph.vaddr 0x%x] [ph.off 0x%x] [ph.filesz 0x%x] [ph.memsz 0x%x]\n", ph.vaddr, ph.off, ph.filesz, ph.memsz);
     if(ph.type != ELF_PROG_LOAD)
       continue;
     if(ph.memsz < ph.filesz)
       goto bad;
     if(ph.vaddr + ph.memsz < ph.vaddr)
       goto bad;
-    if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
-      goto bad;
+    sz = ph.vaddr + ph.memsz;
+    curproc->text_end = ph.vaddr + ph.filesz;
+    curproc->data_end = PGROUNDUP(ph.vaddr + ph.memsz);
+    for(int j = 0; j <= PDX(ph.memsz); j++){
+      pde = &pgdir[j];
+      if((*pde & PTE_P) == 0){
+	if((pgtab = (pte_t*)kalloc()) == 0)
+	  goto bad;
+	memset(pgtab, 0, PGSIZE);
+	*pde = V2P(pgtab) | PTE_P | PTE_W | PTE_U;
+      }
+    }
+    //if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
+    //  goto bad;
     if(ph.vaddr % PGSIZE != 0)
       goto bad;
-    if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
-      goto bad;
+    //if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
+    //  goto bad;
   }
   iunlockput(ip);
   end_op();
@@ -102,6 +118,16 @@ exec(char *path, char **argv)
     if(*s == '/')
       last = s+1;
   safestrcpy(curproc->name, last, sizeof(curproc->name));
+
+  for(last=s=path; *s; s++);
+  safestrcpy(curproc->filename, last, sizeof(curproc->filename));
+
+  /*
+  cprintf(curproc->name);
+  cprintf("\n");
+  cprintf(curproc->filename);
+  cprintf("\n");
+  */
 
   // Commit to the user image.
   oldpgdir = curproc->pgdir;
